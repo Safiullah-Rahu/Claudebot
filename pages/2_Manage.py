30c00985cf85248d029730ccef433b2dc05469ae
Safiullah Rahu <safiullahrahu1555@gmail.com>
2023-10-17 03:12:33 +0500

    Update 2_Manage.py

--- pages/2_Manage.py
+++ pages/2_Manage.py
@@ -75,52 +75,52 @@ def manage_chat():
     if manage_opts == "Create New Index":
         st.header("Create New Index to Upload Documents")
         st.write("---")
-        col1, col2 = st.columns([1,1])
-        doc_ = col1.checkbox("Upload Documents [PDF/TXT/DOCX]")
+        # col1, col2 = st.columns([1,1])
+        # doc_ = col1.checkbox("Upload Documents [PDF/TXT/DOCX]")
         #url_ = col2.checkbox("Upload Website Content [URL]")
         # Prompt the user to upload PDF/TXT files
-        try:
-            if doc_:
-                st.write("Upload PDF/TXT/DOCX Files:")
-                uploaded_files = st.file_uploader("Upload", type=["pdf", "txt", "docx"], label_visibility="collapsed", accept_multiple_files = True)
-                if uploaded_files != []:
-                    st.info('Initializing Document Loading...')
-                    documents = load_docs(uploaded_files)
-                    text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
-                    docs = text_splitter.create_documents(documents)
-                    st.success("Document Loaded Successfully!")
+        # try:
+        #     if doc_:
+        st.write("Upload PDF/TXT/DOCX Files:")
+        uploaded_files = st.file_uploader("Upload", type=["pdf", "txt", "docx"], label_visibility="collapsed", accept_multiple_files = True)
+        if uploaded_files != []:
+            st.info('Initializing Document Loading...')
+            documents = load_docs(uploaded_files)
+            text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
+            docs = text_splitter.create_documents(documents)
+            st.success("Document Loaded Successfully!")
 
-            # Initialize OpenAI embeddings
-            embeddings = OpenAIEmbeddings(model = 'text-embedding-ada-002')
-            #embeddings = HuggingFaceInstructEmbeddings(model_name="hkunlp/instructor-xl")
-            #st.info("Embeddings model loaded!")
-            # Display the uploaded file content
-            file_container = st.expander(f"Click here to see your uploaded content:")
-            file_container.write(docs)
-
+        # Initialize OpenAI embeddings
+        embeddings = OpenAIEmbeddings(model = 'text-embedding-ada-002')
+        #embeddings = HuggingFaceInstructEmbeddings(model_name="hkunlp/instructor-xl")
+        #st.info("Embeddings model loaded!")
+        # Display the uploaded file content
+        file_container = st.expander(f"Click here to see your uploaded content:")
+        file_container.write(docs)
+    
+        # Display success message
+        # st.success("Document Loaded Successfully!")
+        pinecone_index = st.text_input("Enter the name of Index: ")
+        if pinecone_index != "":
+            st.info('Initializing Index Creation...')
+            # Create a new Pinecone index
+            pinecone.create_index(
+                    name = pinecone_index,
+                    metric = 'cosine',
+                    dimension = 1536 #512   # 1536 dim of text-embedding-ada-002
+                    )
+            st.success('Index Successfully Created!')
+    
+            st.info('Initializing Document Uploading to DB...')
+            time.sleep(80)
+            
+            # Upload documents to the Pinecone index
+            vector_store = Pinecone.from_documents(docs, embeddings, index_name=pinecone_index)
+            
             # Display success message
-            # st.success("Document Loaded Successfully!")
-            pinecone_index = st.text_input("Enter the name of Index: ")
-            if pinecone_index != "":
-                st.info('Initializing Index Creation...')
-                # Create a new Pinecone index
-                pinecone.create_index(
-                        name = pinecone_index,
-                        metric = 'cosine',
-                        dimension = 1536 #512   # 1536 dim of text-embedding-ada-002
-                        )
-                st.success('Index Successfully Created!')
-
-                st.info('Initializing Document Uploading to DB...')
-                time.sleep(80)
-                
-                # Upload documents to the Pinecone index
-                vector_store = Pinecone.from_documents(docs, embeddings, index_name=pinecone_index)
-                
-                # Display success message
-                st.success("Document Uploaded Successfully!")
-        except:
-            st.toast("Select any one option to proceed.")
+            st.success("Document Uploaded Successfully!")
+        # except:
+        #     st.toast("Select any one option to proceed.")
 
     elif manage_opts == "Use Existing Index":
         st.header("Use Existing Index to Upload Documents")
